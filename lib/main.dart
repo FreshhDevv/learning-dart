312ee34b25f210428cb26f888042cec54572b978
FreshDev <billyhans90@gmail.com>
2022-06-01 17:42:31 +0100

    Sets in Dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,11 @@ void main() {
 }
 
 void test() {
-  final names = ['Foo', 'Bar', 'Baz'];
-  final foo = names[2];
-  print(foo);
-  final length = names.length;
-  print(names.length);
+  var names = {'foo', 'bar', 'baz'};
+  names.add('foo');
+  names.add('bar');
+  names.add('baz');
+  print(names);
 }
 
 class MyApp extends StatelessWidget {
